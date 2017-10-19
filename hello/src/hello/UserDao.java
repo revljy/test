24c739cb74c8f4d6b881ac3cc3a14c83d7a0276a
ljy <rev@naver.com>
2017-10-19 15:24:48 +0900

    interface add, html append

--- hello/src/hello/UserDao.java
+++ hello/src/hello/UserDao.java
@@ -3,5 +3,6 @@ package hello;
 public class UserDao {
 	public static void main(String[] args) {
 		System.out.println("dao¾ä");
+		System.out.println("wlwlwl");
 	}
 }
---
+++ hello/src/hello/UserDaoInf.java
@@ -0,0 +1,5 @@
+package hello;
+
+public interface UserDaoInf {
+	public void add(String name);
+}
